1edf285800e5d2dd8f6345ccb2f4106da86412f8
taka <shimakuratakanao@gmail.com>
2023-03-05 17:09:34 +0900

    add user login func

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM golang:latest
 RUN mkdir /app
 ADD . /app/
 WORKDIR /app
-RUN go install net/http
+
 CMD ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -1,13 +1,67 @@
 package main
 
-import "net/http"
-import "fmt"
+import (
+    "net/http"
+    "fmt"
+	"encoding/json"
+)
+
+type loginRequest struct {
+	Name string `json:"name"`
+	Password string `json:"password"`
+}
 
 func main() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", loginHandler)
     http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	isValid := validateUser("exampleName", "examplePassword")
+	if isValid {
+		fmt.Println("User is valid")
+	} else {
+    fmt.Println("User is invalid")
+	}
     fmt.Fprint(w, "Hello, World!")
+}
+
+
+
+func validateUser(name, password string) bool {
+    // 適当に決めた文字列
+    correctName := "exampleName"
+    correctPassword := "examplePassword"
+
+    if name == correctName && password == correctPassword {
+        return true
+    }
+    return false
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// JSON形式のリクエストボディをパースする
+	var reqBody loginRequest
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// パースしたリクエストボディをそのままレスポンスとして返す
+	respBody, err := json.Marshal(reqBody)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(respBody)
 }
